29497ed3829ddd7e6c0dd84729b7d3edbaa7bae7
Rigo <rigothedev@gmail.com>
2024-04-11 03:14:29 -0700

    Add nonce generation to `EncryptionType`

--- src/encryption_type.rs
+++ src/encryption_type.rs
@@ -1,3 +1,7 @@
+use hmac::{Hmac, Mac};
+use sha2::Sha256;
+use rand::Rng as _;
+
 use crate::{key_derivation, config::CONFIG};
 
 mod private {
@@ -10,6 +14,9 @@ mod private {
 pub trait EncryptionType: private::Sealed {
     /// Returns the appropriate key for the encryption type.
     fn key() -> [u8; 32];
+
+    /// Generates a 96-bit nonce to encrypt a payload.
+    fn generate_nonce_for(payload: &[u8]) -> [u8; 12];
 }
 
 #[derive(Debug)]
@@ -18,6 +25,13 @@ impl EncryptionType for Deterministic {
     fn key() -> [u8; 32] {
         key_derivation::derive_from(&CONFIG.deterministic_key)
     }
+
+    fn generate_nonce_for(payload: &[u8]) -> [u8; 12] {
+        let mut mac = Hmac::<Sha256>::new_from_slice(&Deterministic::key()).unwrap();
+        mac.update(payload);
+
+        mac.finalize().into_bytes()[0..12].try_into().unwrap()
+    }
 }
 
 #[derive(Debug)]
@@ -25,5 +39,56 @@ pub struct Randomized;
 impl EncryptionType for Randomized {
     fn key() -> [u8; 32] {
         key_derivation::derive_from(&CONFIG.randomized_key)
+    }
+
+    fn generate_nonce_for(_payload: &[u8]) -> [u8; 12] {
+        let mut buffer = [0; 12];
+        rand::thread_rng().fill(&mut buffer);
+
+        buffer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::{testing, utilities::base64};
+
+    mod deterministic {
+        use super::*;
+
+        #[test]
+        fn nonce_is_deterministic() {
+            testing::setup();
+
+            let nonce = Deterministic::generate_nonce_for("rigo is cool".as_bytes());
+
+            // Test that the nonce is 12 bytes long.
+            assert_eq!(nonce.len(), 12);
+
+            // Test that the nonce is deterministic.
+            assert_eq!(nonce, *base64::decode("3gCtDpVQCVyV6Pyg").unwrap());
+        }
+    }
+
+    mod randomized {
+        use super::*;
+
+        #[test]
+        fn nonce_is_randomized() {
+            testing::setup();
+
+            let payload = "much secret much secure".as_bytes();
+            let first_nonce = Randomized::generate_nonce_for(payload);
+            let second_nonce = Randomized::generate_nonce_for(payload);
+
+            // Test that the nonces are 12 bytes long.
+            assert_eq!(first_nonce.len(), 12);
+            assert_eq!(second_nonce.len(), 12);
+
+            // Test that the nonces never match, even when generated for the same payload.
+            assert_ne!(first_nonce, second_nonce);
+        }
     }
 }
